03994353f33bc65bb709fb60a4695ca8b954cfe7
Max Normand <maxnormand@oliviers-MacBook-Pro.local>
2022-11-29 14:38:29 +1100

    set up rspec, rubocop, create first model and schema

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  firstname  :string
+#  lastname   :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :user do
+    
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  firstname  :string
+#  lastname   :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
