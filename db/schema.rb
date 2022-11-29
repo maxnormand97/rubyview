7c838f09c519cb2bdbabf4f5b2c3ef77ec42b272
Max Normand <maxnormand@oliviers-MacBook-Pro.local>
2022-11-29 15:35:30 +1100

    revert model due to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_043041) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_042757) do
   create_table "answers", force: :cascade do |t|
     t.string "label"
     t.string "type"
@@ -22,14 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_043041) do
 
   create_table "questions", force: :cascade do |t|
     t.string "label"
-    t.string "description"
-    t.string "state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title"
     t.string "description"
     t.string "state"
     t.datetime "created_at", null: false
