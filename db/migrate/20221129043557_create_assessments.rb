4294257a93e45d0a088916bb4689b5bc8b2263da
Max Normand <maxnormand@oliviers-MacBook-Pro.local>
2022-11-29 15:39:02 +1100

    new model for tests called assessments

---
+++ db/migrate/20221129043557_create_assessments.rb
@@ -0,0 +1,11 @@
+class CreateAssessments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :assessments do |t|
+      t.string :title
+      t.string :description
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_042757) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_043557) do
   create_table "answers", force: :cascade do |t|
     t.string "label"
     t.string "type"
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_042757) do
     t.datetime "updated_at", null: false
     t.integer "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "assessments", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
