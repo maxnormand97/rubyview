ec835736dad1a359f06e558c6d2824db66863dbe
Max Normand <maxnormand@oliviers-MacBook-Pro.local>
2022-12-02 20:51:09 +1100

    rubocop

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,29 @@
-class QuestionsController < ApplicationController
+# frozen_string_literal: true
 
+class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
 
+  # rubocop:disable Layout/LineLength
   def create
     @question = Question.new(question_params)
 
     if @question.save
       respond_to do |format|
         # format.html { redirect_to edit_assessment_path(id: params[:assessment_id]), notice: "question was successfully created." }
-        format.html { redirect_to assessments_path, notice: "question was successfully created." }
+        format.html { redirect_to assessments_path, notice: 'question was successfully created.' }
         format.turbo_stream
       end
     else
       render :new, status: :unprocessable_entity
     end
   end
+  # rubocop:enable Layout/LineLength
 
   private
 
   def question_params
     params.require(:question).permit(:label, :description, :question_type)
   end
-
 end
